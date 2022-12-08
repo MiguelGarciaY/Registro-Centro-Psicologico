d7beba7c452fd973a4512614188f8eca9e35118c
Miguelgarci4 <migue_garci4@hotmail.com>
2022-12-08 17:56:50 -0500

    Cambios de login 2

---
+++ titulacion-common/src/test/java/com/titulacion/TitulacionCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titulacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitulacionCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
